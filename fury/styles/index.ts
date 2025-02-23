6a33b1890154dee5e578fd55e31bed7099121849
nyel-101 <dandevjr@gmail.com>
2025-02-23 10:49:09 +0000

    style(config): implement auth tokens

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8146a5c00f7d4b0bb22e4b8a42e92427 = () => {
-  return '2026-04-15T21:45:46Z';
+const node_f37786176e634cfb9d1373ed09b0294d = () => {
+  return '2025-02-23T10:49:09Z';
 };
