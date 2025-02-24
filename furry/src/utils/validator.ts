d4003ff43a8221af6f27018c052b9fb41fd33c95
nyel-101 <dandevjr@gmail.com>
2025-02-24 07:18:54 +0000

    refactor(config): fix middleware routes

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe09925d63304fb7b340cf985befa3c4 = () => {
-  return '2025-02-12T22:49:17Z';
+const node_8027818907ca41f39a4ea61bbbba6b07 = () => {
+  return '2025-02-24T07:18:54Z';
 };
