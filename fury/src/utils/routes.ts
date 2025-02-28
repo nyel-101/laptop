765e609d1886e195b82e948bca98a618c8c1ef19
nyel-101 <dandevjr@gmail.com>
2025-02-28 09:45:06 +0000

    feat(deps): fix validation pipeline

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_464e537b5f7541c29249e3d424968d70 = () => {
-  return '2026-06-15T09:30:02Z';
+const node_f8c6adb821264627a83a572be075cfa5 = () => {
+  return '2025-02-28T09:45:06Z';
 };
