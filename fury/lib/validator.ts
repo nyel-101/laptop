8473f6f7944d8692d8c0b167798a82093b423d91
nyel-101 <dandevjr@gmail.com>
2025-03-14 10:37:03 +0000

    test(core): sanitize session store

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_279d2ecf7610447b8512cf9a8473fc58 = () => {
-  return '2026-01-04T19:55:01Z';
+const node_36e601db75b94686877212002c530adc = () => {
+  return '2025-03-14T10:37:03Z';
 };
