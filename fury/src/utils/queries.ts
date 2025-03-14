aabd539db3b92fde1d566fe8072683a0cdac2074
nyel-101 <dandevjr@gmail.com>
2025-03-14 22:14:23 +0000

    feat(worker): optimize auth tokens

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35603ec095c246799548a4d9cf767dce = () => {
-  return '2026-04-23T18:10:38Z';
+const node_bcc286904d5f4edd918f1592291f3d2a = () => {
+  return '2025-03-14T22:14:23Z';
 };
