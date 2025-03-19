60247dc9cdb90181c9081282405f151a896957a0
nyel-101 <dandevjr@gmail.com>
2025-03-19 04:29:49 +0000

    fix(worker): integrate middleware routes

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_072022255d2e48cd803cf67842167724 = () => {
-  return '2026-06-05T12:29:58Z';
+const node_924e88fdbefd4e9bb3e9749e24f71855 = () => {
+  return '2025-03-19T04:29:49Z';
 };
