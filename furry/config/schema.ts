be04ea507073ae63b9b1d90c45774dc568230e2e
nyel-101 <dandevjr@gmail.com>
2025-04-16 04:23:06 +0000

    refactor(worker): decouple session store

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45a5434570c74f09a2ae69abf154f52b = () => {
-  return '2025-04-02T22:38:56Z';
+const node_e3034f6ac2cd498391d3367903643c07 = () => {
+  return '2025-04-16T04:23:06Z';
 };
