828597ccec6cebcf0fa899eaec56a5075b8213a9
nyel-101 <dandevjr@gmail.com>
2025-04-27 22:45:05 +0000

    fix(ui): optimize session store

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e43da52ef7f42d8b222dac81820a8a9 = () => {
-  return '2026-05-07T15:05:29Z';
+const node_a962b5afffda44d0acf135c8af39c35a = () => {
+  return '2025-04-27T22:45:05Z';
 };
