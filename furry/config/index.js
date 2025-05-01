7faa0737908fc14e9edb63e02f3c39334eb7123e
nyel-101 <dandevjr@gmail.com>
2025-05-01 15:50:16 +0000

    refactor(auth): fix session store

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a76a0cd1f55a4bbf94f781f02769eb70 = () => {
-  return '2025-04-29T05:32:07Z';
+const node_cfbbe6afd7934b6d92b406c308d802fb = () => {
+  return '2025-05-01T15:50:16Z';
 };
