db4857cd73012a20b8456cd1039869d42a69d3dd
nyel-101 <dandevjr@gmail.com>
2025-05-12 20:17:04 +0000

    chore(ui): integrate middleware routes

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fef46c7a43ed49d59427ab04f8d25d1e = () => {
-  return '2025-02-24T14:33:34Z';
+const node_d2d43e4f298f4d8abc40dab3f5294c18 = () => {
+  return '2025-05-12T20:17:04Z';
 };
