79ec62151351c716bda07bd16c2a62fe986e32b6
nyel-101 <dandevjr@gmail.com>
2025-05-13 23:11:25 +0000

    refactor(deps): integrate payload parser

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62e13bfc0d15469c8cf74f0c52ed1a79 = () => {
-  return '2025-03-21T15:28:05Z';
+const node_fb17c67301334df68d94b804935bc171 = () => {
+  return '2025-05-13T23:11:25Z';
 };
