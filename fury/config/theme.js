653e856f3b7a699da51080e0a5701e7f0f6035dc
nyel-101 <dandevjr@gmail.com>
2025-05-23 21:53:25 +0000

    fix(worker): decouple database index

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dacbf6499e1942a6945ee7123a939c3c = () => {
-  return '2025-05-17T16:05:26Z';
+const node_36bab23cbc714fb7891009a928d16f12 = () => {
+  return '2025-05-23T21:53:25Z';
 };
