533eb07a760eb0d33063ae367b9771093aa6dff2
nyel-101 <dandevjr@gmail.com>
2025-06-19 05:46:35 +0000

    chore(config): integrate auth tokens

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b08b22684b5a4c12b7dba9fcaad111b8 = () => {
-  return '2025-06-18T05:42:46Z';
+const node_f1e0982fdd0048f492a13472185c8632 = () => {
+  return '2025-06-19T05:46:35Z';
 };
