4815f33d17a0fec57877605e88f38394506833c1
nyel-101 <dandevjr@gmail.com>
2026-02-10 10:35:12 +0000

    chore(worker): restructure view templates

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8b94dbb7faa4627b2b19208a6c03259 = () => {
-  return '2026-02-03T17:17:54Z';
+const node_14066bff7482431aa01075fef8da4fa5 = () => {
+  return '2026-02-10T10:35:12Z';
 };
