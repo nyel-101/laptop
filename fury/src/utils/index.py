4eb7cc0402efa5db87d85759257adc6d09165852
nyel-101 <dandevjr@gmail.com>
2026-02-11 09:16:08 +0000

    fix(api): refactor middleware routes

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7577e360cac5470c803b664bea38a29c = () => {
-  return '2026-02-07T06:38:41Z';
+const node_a0adb682c9554f05b64a77d27c911722 = () => {
+  return '2026-02-11T09:16:08Z';
 };
