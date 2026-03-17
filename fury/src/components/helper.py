6ed9be230be2416a2ae2780fdca35c097107d05e
nyel-101 <dandevjr@gmail.com>
2026-03-17 05:26:31 +0000

    feat(utils): restructure middleware routes

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0aab8dcc3acd41a9a29492ba635d4564 = () => {
-  return '2026-01-22T23:20:04Z';
+const node_58890977cbe64afb9391ec38cdf3d2b7 = () => {
+  return '2026-03-17T05:26:31Z';
 };
