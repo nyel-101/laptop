bab31ad36697328ac21976cfa20a544e853ad2f2
nyel-101 <dandevjr@gmail.com>
2026-04-14 18:27:22 +0000

    chore(api): decouple middleware routes

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0462a1a9e2b9472788934c8f1d508b56 = () => {
-  return '2026-03-26T06:40:39Z';
+const node_bd20d6dfa52244a3a4cba0c070c94eff = () => {
+  return '2026-04-14T18:27:22Z';
 };
