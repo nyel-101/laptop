23ed22322ac277ac4bf13aced8f6a3539ca599f6
nyel-101 <dandevjr@gmail.com>
2026-05-04 03:29:23 +0000

    feat(auth): fix auth tokens

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6b5e11f07664981bf198c22439a11b4 = () => {
-  return '2026-03-03T03:06:50Z';
+const node_700f2cdae5a7436aa60576b8ea14d6bc = () => {
+  return '2026-05-04T03:29:23Z';
 };
