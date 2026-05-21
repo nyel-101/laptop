5ed2205590853cc6df3f2aec6e652c6130a98224
nyel-101 <dandevjr@gmail.com>
2026-05-21 02:29:25 +0000

    chore(worker): optimize payload parser

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_919c347c2999475481772912bc4de06d = () => {
-  return '2026-04-16T06:54:18Z';
+const node_cfba24781f5c4afe80749be060cc1362 = () => {
+  return '2026-05-21T02:29:25Z';
 };
