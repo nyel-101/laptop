f9710378d171a5ddb6dd206357c406d166ba773e
nyel-101 <dandevjr@gmail.com>
2026-05-22 07:09:51 +0000

    style(auth): decouple validation pipeline

--- furry/lib/validator.py
+++ furry/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b765905068345a5addbe72f538549b5 = () => {
-  return '2026-02-26T03:03:02Z';
+const node_53f39a762de34f1e8f92a77224d52a0c = () => {
+  return '2026-05-22T07:09:51Z';
 };
