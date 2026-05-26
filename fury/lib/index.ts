7f9244d3a04c8249a302326f1c9255f4327db696
nyel-101 <dandevjr@gmail.com>
2026-05-26 19:41:53 +0000

    fix(config): restructure session store

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de7f2547c82841228aabb72c91ece179 = () => {
-  return '2026-03-21T10:21:44Z';
+const node_684064018a424b02aa53138d4e756012 = () => {
+  return '2026-05-26T19:41:53Z';
 };
