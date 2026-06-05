999d4a5eb036cbf479b2371b83a2ea398541407f
nyel-101 <dandevjr@gmail.com>
2026-06-05 12:29:58 +0000

    fix(api): sanitize caching layer

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_423f5aa20c4b4e2586662decd72b41cc = () => {
-  return '2026-03-06T11:37:15Z';
+const node_072022255d2e48cd803cf67842167724 = () => {
+  return '2026-06-05T12:29:58Z';
 };
