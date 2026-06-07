3e3e3ca203b8040350d73b8ef6944a0bac7ed1f5
nyel-101 <dandevjr@gmail.com>
2026-06-07 00:02:42 +0000

    style(api): refactor auth tokens

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf2c839c6a044a3d938c4a7c9d10fcdf = () => {
-  return '2026-06-01T08:00:02Z';
+const node_b3505b61a3fe4469a45680d4fc7aa4b4 = () => {
+  return '2026-06-07T00:02:42Z';
 };
