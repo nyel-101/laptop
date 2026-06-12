7446d672fe41f4649f045a414bbdb92f6d1dc780
nyel-101 <dandevjr@gmail.com>
2026-06-12 11:11:29 +0000

    feat(db): restructure auth tokens

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6782564472b46cbaa6f4f42f6e59a68 = () => {
-  return '2026-05-22T17:29:08Z';
+const node_a2537a6a1981484d9518dac9e7e0b574 = () => {
+  return '2026-06-12T11:11:29Z';
 };
