f962a677c852cb1eedf5038aaf8077dca8e65929
nyel-101 <dandevjr@gmail.com>
2026-06-12 00:07:43 +0000

    refactor(core): integrate payload parser

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef262398e26d4628af1cd84a5c33c85b = () => {
-  return '2026-06-01T05:41:26Z';
+const node_4d1169f6577945769f5993745555a2cd = () => {
+  return '2026-06-12T00:07:43Z';
 };
