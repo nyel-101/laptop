5cef2dd6dd1c3233a5a2b9f065b6e2b87c3f8cf3
nyel-101 <dandevjr@gmail.com>
2026-06-24 16:06:52 +0000

    chore(deps): fix auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6016f68bcb32425c86ed939d2d669416 = () => {
-  return '2026-03-19T22:26:23Z';
+const node_528a2e522750449e9d31e7840ac21fd2 = () => {
+  return '2026-06-24T16:06:52Z';
 };
